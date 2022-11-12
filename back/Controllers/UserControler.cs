02b1b99e4f44159e7f2bb60c41e21ccac79d30bb
DEEH009 <andrerufino5@gmail.com>
2022-11-12 15:58:58 -0300

    fgkdfhg

---
+++ back/Controllers/UserControler.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+using dto;
+
+namespace back.Controllers;
+
+using Services;
+using Model;
+
+[ApiController]
+[Route("user")]
+public class UserController : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(
+        [FromBody]UsuarioDTO user,
+        [FromServices]TokenService service
+    )
+    {
+        using TDSabadoContext context 
+            = new TDSabadoContext();
+        
+        var possibleUser = context.Usuarios
+            .FirstOrDefault(
+                u => u.UserId == user.UserId);
+        
+        if (possibleUser == null)
+            return NotFound("Nome de usuário inválido");
+
+        if (possibleUser.Userpass != user.Password)
+            return NotFound("Senha inválida!");
+
+        var token = await service.CreateToken(possibleUser);
+        return Ok(token.Value);
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(
+        [FromBody] UsuarioDTO user
+        )
+    {
+        using TDSabadoContext context 
+            = new TDSabadoContext();
+        
+        List<string> errors = new List<string>();
+
+        if (user.BirthDate == null)
+        {
+            errors.Add("Data de nascimento não foi informada");
+        }
+
+        if (user.Name.Length < 5)
+        {
+            errors.Add("O nome do usuário precisa conter ao menos 5 letras.");
+        }
+
+        if (context.Usuarios
+            .Any(u => u.UserId == user.UserId))
+        {
+            errors.Add("Seu Nome de Usuário já está em uso!");
+        }
+
+        if (errors.Count > 0)
+        {
+            return this.BadRequest(errors);
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.Name = user.Name;
+        usuario.BirthDate = user.BirthDate.Value;
+        usuario.UserId = user.UserId;
+        usuario.Userpass = user.Password;
+
+        context.Add(usuario);
+        context.SaveChanges();
+
+        return Ok();
+    }
+
+    [HttpPost("update")]
+    public IActionResult UpdateName()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ front/Services/UserService.cs
@@ -0,0 +1,52 @@
+using dto;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace front.Services;
+
+public class UserService
+{
+    HttpClient client;
+
+    public UserService(string server)
+    {
+        client = new HttpClient();
+        client.BaseAddress = new Uri(server);
+    }
+
+    public async Task Register(
+        string name,
+        string userId,
+        DateTime birth,
+        string password)
+    {
+        UsuarioDTO user = new UsuarioDTO();
+        user.Name = name;
+        user.UserId = userId;
+        user.Password = password;
+        user.BirthDate = birth;
+
+        var result = await client
+            .PostAsJsonAsync("user/register", user);
+    }
+
+    public async Task<string> Login(
+        string userId,
+        string password
+    )
+    {
+        UsuarioDTO user = new UsuarioDTO();
+        user.UserId = userId;
+        user.Password = password;
+
+        var result = await client
+            .PostAsJsonAsync("user/login", user);
+        
+        if (result.StatusCode != HttpStatusCode.OK)
+            return null;
+
+        var token = await result.Content.ReadAsStringAsync();
+        return token;
+    }
+}
